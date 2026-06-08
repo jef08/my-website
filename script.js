59645f42485bd014269dd06f2f8c2eaa2869b2b1
jef08 <jasfielding@gmail.com>
2026-06-08 16:30:06 +0200

    Attempting to fix Safari responsiveness. Added feature to close navbar.

--- script.js
+++ script.js
@@ -20,6 +20,16 @@ function showView(viewId) {
     }
 }
 
+//Collapse navbar when clicking outside of navbar (mobile view)//
+document.addEventListener('click', function(e) {
+    const navbar = document.getElementById('navbar');
+    const menu = document.getElementById('navbarNav');
+
+    if(!navbar.contains(e.target) && menu.classList.contains('show')) {
+        bootstrap.Collapse.getInstance(menu).hide();
+    }
+})
+
 document.getElementById('hero-btn').addEventListener('click', () => showView('hero'));
 document.getElementById('about-btn').addEventListener('click', () => showView('about'));
 document.getElementById('skills-btn').addEventListener('click', () => showView('skills'));
